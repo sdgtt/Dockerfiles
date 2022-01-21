c78db0ebd9dbd5b3460ad26443ba9d56fa711c1c
Travis F. Collins <travis.collins@analog.com>
2022-01-21 10:40:20 -0800

    Tfcollins arm fix push (#5)
    * Update build_dev.yml
    
    * Update build_dev.yml
    
    * Update ci-ubuntu.sh
    
    * Update build_dev.yml

--- libiio/ci-ubuntu.sh
+++ libiio/ci-ubuntu.sh
@@ -4,4 +4,6 @@ uname -a
 DEBIAN_FRONTEND=noninteractive apt update
 DEBIAN_FRONTEND=noninteractive apt -y upgrade
 DEBIAN_FRONTEND=noninteractive apt install -y bison flex cmake git build-essential libxml2-dev doxygen
+DEBIAN_FRONTEND=noninteractive apt install -y libaio-dev libusb-1.0-0-dev
+DEBIAN_FRONTEND=noninteractive apt install -y libserialport-dev libavahi-client-dev
 DEBIAN_FRONTEND=noninteractive apt install -y python3 python3-sphinx python3-setuptools
