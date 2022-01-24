26546800aa7fe0dc5503749e772cb516515d7de0
Travis F. Collins <travis.collins@analog.com>
2022-01-24 14:30:13 -0800

    Update ml_build.sh

--- ml_build.sh
+++ ml_build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Build cores
+/usr/local/MATLAB/R2021a/bin/matlab -nodesktop -nodisplay -nosplash -r 'setupzynqradiorepositories();exit(0);'
+
+# Build container
 mkdir -p mlhsp
 mount --bind /mlhsp mlhsp
 
