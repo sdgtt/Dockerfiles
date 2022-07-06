e1d1cdc7190f9059b65b8ba3e97a74c8ac2dacbf
Travis F. Collins <travis.collins@analog.com>
2022-07-06 17:54:29 -0600

    Add libssl to build
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- libiio/ci-ubuntu.sh
+++ libiio/ci-ubuntu.sh
@@ -3,7 +3,7 @@ set -x
 uname -a
 DEBIAN_FRONTEND=noninteractive apt update
 DEBIAN_FRONTEND=noninteractive apt -y upgrade
-DEBIAN_FRONTEND=noninteractive apt install -y git sudo wget
+DEBIAN_FRONTEND=noninteractive apt install -y git sudo wget libssl-dev
 DEBIAN_FRONTEND=noninteractive apt install -y bison flex cmake git build-essential libxml2-dev doxygen
 DEBIAN_FRONTEND=noninteractive apt install -y libaio-dev libusb-1.0-0-dev
 DEBIAN_FRONTEND=noninteractive apt install -y libserialport-dev libavahi-client-dev
