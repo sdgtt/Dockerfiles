a12a6a18d2365d404efd4872df233be287bad359
Travis F. Collins <travis.collins@analog.com>
2022-08-17 11:23:36 -0600

    Add libzstd-dev to ARM builds
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- libiio/ci-ubuntu.sh
+++ libiio/ci-ubuntu.sh
@@ -5,7 +5,7 @@ DEBIAN_FRONTEND=noninteractive apt update
 DEBIAN_FRONTEND=noninteractive apt -y upgrade
 DEBIAN_FRONTEND=noninteractive apt install -y git sudo wget libssl-dev
 DEBIAN_FRONTEND=noninteractive apt install -y bison flex cmake git build-essential libxml2-dev doxygen
-DEBIAN_FRONTEND=noninteractive apt install -y libaio-dev libusb-1.0-0-dev
+DEBIAN_FRONTEND=noninteractive apt install -y libaio-dev libusb-1.0-0-dev libzstd-dev
 DEBIAN_FRONTEND=noninteractive apt install -y libserialport-dev libavahi-client-dev
 DEBIAN_FRONTEND=noninteractive apt install -y python3 python3-sphinx python3-setuptools
 
