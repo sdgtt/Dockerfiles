6de43d6312437305e4a97e2afba1f1ba5bcaa355
Trecia Agoylo <trecia.agoylo@analog.com>
2023-06-02 14:30:43 +0800

    Fixes on installation script
    Signed-off-by: Trecia Agoylo <trecia.agoylo@analog.com>

--- test-harness/install_nebula.sh
+++ test-harness/install_nebula.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone -b 'master' --single-branch --depth 1 https://github.com/sdgtt/nebula.git
 cd nebula
-pip install -r requirements.txt -y
+pip install -r requirements.txt
 python setup.py install
 cd ..
