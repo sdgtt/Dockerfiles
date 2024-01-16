302498296cb9183cb96e85c6554e27865b5f3dff
Travis F. Collins <travis.collins@analog.com>
2024-01-16 08:03:24 -0700

    Update master/main branches
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- pyadi-iio/all_python_versions.sh
+++ pyadi-iio/all_python_versions.sh
@@ -1,8 +1,8 @@
-wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/master/.github/scripts/install_libiio.sh -O - | bash
-wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/master/.github/scripts/install_part_libs.sh -O - | bash
-wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/master/.github/scripts/install_iioemu.sh -O - | bash
+wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/main/.github/scripts/install_libiio.sh -O - | bash
+wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/main/.github/scripts/install_part_libs.sh -O - | bash
+wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/main/.github/scripts/install_iioemu.sh -O - | bash
 
-wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/master/requirements.txt
-wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/master/requirements_dev.txt
-wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/master/requirements_doc.txt
-wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/master/.github/scripts/install_pydeps.sh -O - | bash
+wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/main/requirements.txt
+wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/main/requirements_dev.txt
+wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/main/requirements_doc.txt
+wget --timeout=5 https://raw.githubusercontent.com/analogdevicesinc/pyadi-iio/main/.github/scripts/install_pydeps.sh -O - | bash
