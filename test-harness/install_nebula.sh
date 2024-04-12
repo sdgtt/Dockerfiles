8915d6df400a9fd6b6d66ec9d2937968740efb81
Trecia Agoylo <trecia.agoylo@analog.com>
2024-04-12 13:59:35 +0800

    use main branch of nebula
    Signed-off-by: Trecia Agoylo <trecia.agoylo@analog.com>

--- test-harness/install_nebula.sh
+++ test-harness/install_nebula.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone -b 'master' --single-branch --depth 1 https://github.com/sdgtt/nebula.git
+git clone -b 'main' --single-branch --depth 1 https://github.com/sdgtt/nebula.git
 cd nebula
 pip3 install -r requirements.txt
 pip3 install .
