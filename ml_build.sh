de7d437bb4b0fc2272ce55751eff2dcea8d3d374
Travis Collins <Travis.Collins@analog.com>
2025-02-07 11:43:27 -0700

    Update ml build
    Signed-off-by: Travis Collins <Travis.Collins@analog.com>

--- ml_build.sh
+++ ml_build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Build cores
-/usr/local/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath','/opt/Xilinx/Vivado/${2}/bin/vivado');setupzynqradiorepositories();exit(0);"
+/opt/MATLAB/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath','/opt/Xilinx/Vivado/${2}/bin/vivado');setupzynqradiorepositories();exit(0);"
 
 # Build container
-mkdir -p mlhsp
-mount --bind /mlhsp mlhsp
+#mkdir -p mlhsp
+#mount --bind /mlhsp mlhsp
 
 docker build -f Dockerfile-ml -t tfcollins/hdl-ci:latest .
 
-umount mlhsp
+#umount mlhsp
