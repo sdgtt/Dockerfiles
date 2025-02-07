fb2d2ccc25d670a868139912b06aa5f0d50ca072
Travis F. Collins <travis.collins@analog.com>
2025-02-07 11:44:34 -0700

    Update dockerfile to reflect internal rom configs
    Signed-off-by: Travis F. Collins <travis.collins@analog.com>

--- ml_build.sh
+++ ml_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build cores
-/opt/MATLAB/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath','/opt/Xilinx/Vivado/${2}/bin/vivado');setupzynqradiorepositories();exit(0);"
+/opt/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath','/opt/Xilinx/Vivado/${2}/bin/vivado');setupzynqradiorepositories();exit(0);"
 
 # Build container
 #mkdir -p mlhsp
