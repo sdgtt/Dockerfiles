1f4a6b7b2e2791c344164b9edcdb41c9bed96a3f
Travis F. Collins <travis.collins@analog.com>
2025-02-10 15:55:53 -0700

    Update ml_build.sh

--- ml_build.sh
+++ ml_build.sh
@@ -7,8 +7,13 @@ cp -r /opt/MATLAB/HSPs /scratch/
 ls /scratch/HSPs
 
 # Build cores
+export DISPLAY_ID=:$(shuf -i 10-1000 -n 1)
+Xvfb $DISPLAY_ID &
+XVFB_PID=$!
+export DISPLAY=$DISPLAY_ID
 /opt/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "matlabshared.supportpkg.setSupportPackageRoot('/scratch/HSPs/${1}/');exit(0);"
 /opt/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath','/opt/Xilinx/Vivado/${2}/bin/vivado');setupzynqradiorepositories();exit(0);"
+kill -9 $XVFB_PID || true
 
 # Build container
 #mkdir -p mlhsp
