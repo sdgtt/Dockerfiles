56397d325879ddc7f4548791cfc1f3149d1744d6
Travis F. Collins <travis.collins@analog.com>
2025-02-10 15:20:17 -0700

    Update ml_build.sh

--- ml_build.sh
+++ ml_build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -xe
 
+# Create tmp HSP folder
+cp -r /opt/MATLAB/HSPs /scratch/
+
 # Build cores
+/opt/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "matlabshared.supportpkg.setSupportPackageRoot('/scratch/HSPs/${1}/');exit(0);"
 /opt/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath','/opt/Xilinx/Vivado/${2}/bin/vivado');setupzynqradiorepositories();exit(0);"
 
 # Build container
@@ -9,6 +13,7 @@ set -xe
 #mount --bind /mlhsp mlhsp
 
 cp -r /home/tcollins/.matlab .
+mv /scratch/HSPs .
 
 docker build -f Dockerfile-ml -t tfcollins/hdl-ci:latest .
 
