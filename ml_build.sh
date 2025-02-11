492e2e8b283b05f220a11995742e275f466a8804
Travis F. Collins <travis.collins@analog.com>
2025-02-11 13:06:15 -0700

    Update ml_build.sh

--- ml_build.sh
+++ ml_build.sh
@@ -9,7 +9,9 @@ ls /scratch/HSPs
 # Build cores
 export DISPLAY_ID=:$(shuf -i 10-1000 -n 1)
 Xvfb $DISPLAY_ID &
+sleep 5
 XVFB_PID=$!
+sleep 5
 export DISPLAY=$DISPLAY_ID
 /opt/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "matlabshared.supportpkg.setSupportPackageRoot('/opt/MATLAB/HSPs/${1}');exit(0);"
 /opt/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath','/opt/Xilinx/Vivado/${2}/bin/vivado');setupzynqradiorepositories();pause(5);exit(0);"
@@ -26,7 +28,9 @@ docker build -f Dockerfile-ml -t tfcollins/hdl-ci:latest .
 
 export DISPLAY_ID=:$(shuf -i 10-1000 -n 1)
 Xvfb $DISPLAY_ID &
+sleep 5
 XVFB_PID=$!
+sleep 5
 export DISPLAY=$DISPLAY_ID
 docker run -v /opt/MATLAB:/opt/MATLAB:ro --name temp-ci -d tfcollins/hdl-ci:latest tail -f /dev/null
 docker exec temp-ci /bin/bash -c '/opt/MATLAB/R2023b/bin/matlab -nodesktop -nodisplay -nosplash -r "disp(matlabshared.supportpkg.setSupportPackageRoot( \"/HSPs/R2023b\"));exit(0);"'
