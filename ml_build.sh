23e904f60206833deced82e6c25d6e2a5f7113b8
Travis F. Collins <travis.collins@analog.com>
2025-02-11 14:12:28 -0700

    Update ml_build.sh

--- ml_build.sh
+++ ml_build.sh
@@ -13,7 +13,7 @@ sleep 5
 XVFB_PID=$!
 sleep 5
 export DISPLAY=$DISPLAY_ID
-/opt/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "matlabshared.supportpkg.setSupportPackageRoot('/opt/MATLAB/HSPs/${1}');exit(0);"
+#/opt/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "matlabshared.supportpkg.setSupportPackageRoot('/opt/MATLAB/HSPs/${1}');exit(0);"
 /opt/MATLAB/$1/bin/matlab -nodesktop -nodisplay -nosplash -r "hdlsetuptoolpath('ToolName','Xilinx Vivado','ToolPath','/opt/Xilinx/Vivado/${2}/bin/vivado');setupzynqradiorepositories();pause(5);exit(0);"
 kill -9 $XVFB_PID || true
 
@@ -34,10 +34,10 @@ sleep 5
 XVFB_PID=$!
 sleep 5
 export DISPLAY=$DISPLAY_ID
-docker run -v /opt/MATLAB:/opt/MATLAB:ro --name temp-ci -d tfcollins/hdl-ci:latest tail -f /dev/null
-docker exec temp-ci /bin/bash -c '/opt/MATLAB/R2023b/bin/matlab -nodesktop -nodisplay -nosplash -r "disp(matlabshared.supportpkg.setSupportPackageRoot( \"/HSPs/R2023b\"));exit(0);"'
-docker commit temp-ci tfcollins/hdl-ci:latest
-docker rm -f temp-ci
+#docker run -v /opt/MATLAB:/opt/MATLAB:ro --name temp-ci -d tfcollins/hdl-ci:latest tail -f /dev/null
+#docker exec temp-ci /bin/bash -c '/opt/MATLAB/R2023b/bin/matlab -nodesktop -nodisplay -nosplash -r "disp(matlabshared.supportpkg.setSupportPackageRoot( \"/HSPs/R2023b\"));exit(0);"'
+#docker commit temp-ci tfcollins/hdl-ci:latest
+#docker rm -f temp-ci
 
 # Check
 docker run -v /opt/MATLAB:/opt/MATLAB:ro tfcollins/hdl-ci:latest  /bin/bash -c '/opt/MATLAB/R2023b/bin/matlab -nodesktop -nodisplay -nosplash -r "disp(matlabshared.supportpkg.getSupportPackageRoot());pause(5);exit(0);"'
